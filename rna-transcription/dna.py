f01b3be498cf615b1a29f34eae942f6289e7f70b
Catalina De la cuesta <delacuesta@hotmail.com>
2015-11-11 13:42:54 -0500

    Makes a dummy change to test continuous integration in codeship

--- rna-transcription/dna.py
+++ rna-transcription/dna.py
@@ -1,9 +1,11 @@
 #dna Exercism
 from string import maketrans  
 
+
 def to_rna(dna):
-	dna_nucleotides = "GCTA"
-	rna_nucleotides = "CGAU"
-	transtable = maketrans(dna_nucleotides, rna_nucleotides)
-	return dna.translate(transtable)
+    dna_nucleotides = "GCTA"
+    rna_nucleotides = "CGAU"
+    transtable = maketrans(dna_nucleotides, rna_nucleotides)
+    a = 1
+    return dna.translate(transtable)
 
